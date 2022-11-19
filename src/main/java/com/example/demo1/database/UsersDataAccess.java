17017943a7586f42136bd383cd57bc2805429110
othdr <otmax.vft@gmail.com>
2022-11-19 21:27:53 +0100

    minor changes

--- src/main/java/com/example/demo1/database/UsersDataAccess.java
+++ src/main/java/com/example/demo1/database/UsersDataAccess.java
@@ -1,16 +1,11 @@
 package com.example.demo1.database;
 
 import com.example.demo1.models.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 @Repository

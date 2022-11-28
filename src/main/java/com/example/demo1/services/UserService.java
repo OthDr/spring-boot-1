ebe9fe8756a17a2ec07b28530093951fc05ce53a
othdr <otmax.vft@gmail.com>
2022-11-28 22:53:56 +0100

    created user service layer

---
+++ src/main/java/com/example/demo1/services/UserService.java
@@ -0,0 +1,73 @@
+package com.example.demo1.services;
+
+import com.example.demo1.database.UsersDataAccess;
+import com.example.demo1.models.User;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class UserService {
+
+    @Autowired
+    UsersDataAccess usersDataAccess;
+
+
+    public Optional<User> getUserById(String id) {
+        return usersDataAccess.findById(Integer.parseInt(id));
+    }
+
+    public List<User> getUsers() {
+        return usersDataAccess.findAll();
+    }
+
+
+    public List<User> getUserByName(String name) {
+        return usersDataAccess.getByFirstName(name);
+    }
+
+    public void createUser(String userJSON) {
+
+        System.out.println("POST(/create) performed");
+        User user;
+        try {
+            if (userJSON != null) {
+                user = new User(new ObjectMapper().readValue(userJSON, User.class).getFirstname(),
+                        new ObjectMapper().readValue(userJSON, User.class).getLastname()
+                );
+                System.out.println("Request Body: " + userJSON);
+                usersDataAccess.save(user);
+            }
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void updateUser(String userJSON, String id) {
+        User user;
+        if (userJSON != null) {
+            try {
+                user = new User(Integer.parseInt(id),
+                        new ObjectMapper().readValue(userJSON, User.class).getFirstname(),
+                        new ObjectMapper().readValue(userJSON, User.class).getLastname()
+                );
+                System.out.println("UPDATED user with ID: " + user.getId());
+                usersDataAccess.save(user);
+
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public void deleteUser(String id) {
+        System.out.println("DELETED user with ID: " + id);
+        usersDataAccess.deleteById(Integer.parseInt(id));
+    }
+}

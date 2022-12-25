24986083ecb98552cc10dcee18871508e9c9e6a5
othdr <otmax.vft@gmail.com>
2022-12-25 22:38:57 +0100

    switched to Lombok

--- src/main/java/com/example/demo1/models/User.java
+++ src/main/java/com/example/demo1/models/User.java
@@ -1,8 +1,17 @@
 package com.example.demo1.models;
+import lombok.*;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -12,41 +21,5 @@ public class User {
     @Column(name = "lastname")
     private String lastname;
 
-    public User() {
-    }
 
-    public String getFirstname() {
-        return firstname;
-    }
-
-    public User(String firstname, String lastname) {
-        this.firstname = firstname;
-        this.lastname = lastname;
-    }
-
-    public User(int id, String firstname, String lastname) {
-        this.id = id;
-        this.firstname = firstname;
-        this.lastname = lastname;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
 }

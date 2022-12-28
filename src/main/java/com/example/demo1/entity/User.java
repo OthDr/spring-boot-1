df3166796fcd8621e3ee6575f925ff110f73c19c
othdr <otmax.vft@gmail.com>
2022-12-28 22:30:22 +0100

    minor changes

--- src/main/java/com/example/demo1/entity/User.java
+++ src/main/java/com/example/demo1/entity/User.java
@@ -1,7 +1,10 @@
 package com.example.demo1.entity;
+
 import lombok.*;
 
 import javax.persistence.*;
+
+import static javax.persistence.GenerationType.SEQUENCE;
 
 @Entity
 @Table(name = "users")
@@ -14,7 +17,15 @@ import javax.persistence.*;
 
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @SequenceGenerator(
+            name = "user_sequence",
+            sequenceName = "user_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "user_sequence"
+    )
     private int id;
     @Column(name = "firstname")
     private String firstname;
